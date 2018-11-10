560b98aa66182838c87c23eecbdc8e5a603cdd41
Chi Lin <chilin89117@gmail.com>
2018-11-09 17:32:44 -0800

    Extract Prisma secret to env variable

--- src/prisma.js
+++ src/prisma.js
@@ -4,7 +4,7 @@ import {fragmentReplacements} from './resolvers/resolvers';
 const prisma = new Prisma({
   typeDefs: 'src/generated/prisma.graphql',
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: 'supersecretpassword',
+  secret: process.env.PRISMA_SECRET,
   fragmentReplacements
 });
 
