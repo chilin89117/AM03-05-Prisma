f8af07d915b898c49af5cde7848ac87a3667b54c
Chi Lin <chilin89117@gmail.com>
2018-12-01 21:28:57 -0800

    Revert "Delete sample test file"
    This reverts commit 11a311d20cc20df67164abaed64d2efbb144f74c.

---
+++ tests/example.js
@@ -0,0 +1,27 @@
+// Rename this file to 'example.test.js' to run these tests
+import {getFirst, isPwdValid} from '../src/utils/example';
+
+test('should get 1st name', () => {
+  const fname = getFirst('Abbie M.');
+  expect(fname).toBe('Abbie');
+});
+
+test('should return 1st name for one name', () => {
+  const fname = getFirst('Abbie');
+  expect(fname).toBe('Abbie');
+});
+
+test('should reject pwd length < 8', () => {
+  const valid = isPwdValid('abcd');
+  expect(valid).toBe(false);
+});
+
+test('should reject "password" in password', () => {
+  const valid = isPwdValid('abcPassword');
+  expect(valid).toBe(false);
+});
+
+test('should accept valid password', () => {
+  const valid = isPwdValid('abcd1234');
+  expect(valid).toBe(true);
+});
